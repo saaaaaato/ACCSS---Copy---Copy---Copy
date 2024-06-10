b8795d2a0362308916b3c80031c59dd41234ace6
saaaaaato <168072448+saaaaaato@users.noreply.github.com>
2024-06-11 00:29:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import pyotp
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///db/database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
