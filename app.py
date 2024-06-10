5b39fa67d01fac9002ceb06847eb6d0482fb96e0
saaaaaato <168072448+saaaaaato@users.noreply.github.com>
2024-06-11 00:23:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import pyotp
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///db/database.db')
+db = SQLAlchemy(app)
 
-def get_db_connection():
-    conn = sqlite3.connect('db/database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    avatar = db.Column(db.String(200))
+    nickname = db.Column(db.String(200))
+    login = db.Column(db.String(200))
+    password = db.Column(db.String(200))
+    fa_code = db.Column(db.String(32))
+    uid = db.Column(db.String(200))
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    accounts = conn.execute('SELECT * FROM accounts').fetchall()
-    conn.close()
+    accounts = Account.query.all()
     return render_template('index.html', accounts=accounts)
 
 @app.route('/add', methods=('GET', 'POST'))
@@ -26,55 +32,43 @@ def add():
         fa_secret = pyotp.random_base32()
         uid = request.form['uid']
 
-        conn = get_db_connection()
-        conn.execute('INSERT INTO accounts (avatar, nickname, login, password, fa_code, uid) VALUES (?, ?, ?, ?, ?, ?)',
-                     (avatar, nickname, login, password, fa_secret, uid))
-        conn.commit()
-        conn.close()
+        new_account = Account(avatar=avatar, nickname=nickname, login=login, password=password, fa_code=fa_secret, uid=uid)
+        db.session.add(new_account)
+        db.session.commit()
         return redirect(url_for('index'))
 
     return render_template('add.html')
 
 @app.route('/edit/<int:id>', methods=('GET', 'POST'))
 def edit(id):
-    conn = get_db_connection()
-    account = conn.execute('SELECT * FROM accounts WHERE id = ?', (id,)).fetchone()
+    account = Account.query.get(id)
 
     if request.method == 'POST':
-        avatar = request.form['avatar']
-        nickname = request.form['nickname']
-        login = request.form['login']
-        password = request.form['password']
-        fa_secret = request.form['fa_code']
-        uid = request.form['uid']
+        account.avatar = request.form['avatar']
+        account.nickname = request.form['nickname']
+        account.login = request.form['login']
+        account.password = request.form['password']
+        account.fa_code = request.form['fa_code']
+        account.uid = request.form['uid']
 
-        conn.execute('UPDATE accounts SET avatar = ?, nickname = ?, login = ?, password = ?, fa_code = ?, uid = ? WHERE id = ?',
-                     (avatar, nickname, login, password, fa_secret, uid, id))
-        conn.commit()
-        conn.close()
+        db.session.commit()
         return redirect(url_for('index'))
 
     return render_template('edit.html', account=account)
 
 @app.route('/delete/<int:id>', methods=('POST',))
 def delete(id):
-    conn = get_db_connection()
-    conn.execute('DELETE FROM accounts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
+    account = Account.query.get(id)
+    db.session.delete(account)
+    db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/2fa/<int:id>')
 def two_factor_auth(id):
-    conn = get_db_connection()
-    account = conn.execute('SELECT * FROM accounts WHERE id = ?', (id,)).fetchone()
-    conn.close()
-
+    account = Account.query.get(id)
     if account:
-        fa_secret = account['fa_code']
-        totp = pyotp.TOTP(fa_secret)
+        totp = pyotp.TOTP(account.fa_code)
         return totp.now()
-
     return "Account not found", 404
 
 if __name__ == "__main__":
