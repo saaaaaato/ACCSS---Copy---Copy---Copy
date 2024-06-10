6d28263fc48483775fa116298b79aab656401a71
saaaaaato <168072448+saaaaaato@users.noreply.github.com>
2024-06-10 23:41:37 +0300

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 # app.py
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+import os
+import psycopg2
 import pyotp
+import bcrypt
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY', 'supersecretkey')
+csrf = CSRFProtect(app)
 
 def get_db_connection():
-    conn = sqlite3.connect('db/database.db')
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    if DATABASE_URL:
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    else:
+        conn = sqlite3.connect('db/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 @app.route('/')
 def index():
     conn = get_db_connection()
-    accounts = conn.execute('SELECT * FROM accounts').fetchall()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM accounts')
+    accounts = cur.fetchall()
+    cur.close()
     conn.close()
     return render_template('index.html', accounts=accounts)
 
@@ -27,10 +39,14 @@ def add():
         fa_secret = pyotp.random_base32()
         uid = request.form['uid']
 
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
         conn = get_db_connection()
-        conn.execute('INSERT INTO accounts (avatar, nickname, login, password, fa_code, uid) VALUES (?, ?, ?, ?, ?, ?)',
-                     (avatar, nickname, login, password, fa_secret, uid))
+        cur = conn.cursor()
+        cur.execute('INSERT INTO accounts (avatar, nickname, login, password, fa_code, uid) VALUES (%s, %s, %s, %s, %s, %s)',
+                    (avatar, nickname, login, hashed_password, fa_secret, uid))
         conn.commit()
+        cur.close()
         conn.close()
         return redirect(url_for('index'))
 
@@ -39,7 +55,9 @@ def add():
 @app.route('/edit/<int:id>', methods=('GET', 'POST'))
 def edit(id):
     conn = get_db_connection()
-    account = conn.execute('SELECT * FROM accounts WHERE id = ?', (id,)).fetchone()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM accounts WHERE id = %s', (str(id),))
+    account = cur.fetchone()
 
     if request.method == 'POST':
         avatar = request.form['avatar']
@@ -49,9 +67,12 @@ def edit(id):
         fa_secret = request.form['fa_code']
         uid = request.form['uid']
 
-        conn.execute('UPDATE accounts SET avatar = ?, nickname = ?, login = ?, password = ?, fa_code = ?, uid = ? WHERE id = ?',
-                     (avatar, nickname, login, password, fa_secret, uid, id))
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        cur.execute('UPDATE accounts SET avatar = %s, nickname = %s, login = %s, password = %s, fa_code = %s, uid = %s WHERE id = %s',
+                    (avatar, nickname, login, hashed_password, fa_secret, uid, id))
         conn.commit()
+        cur.close()
         conn.close()
         return redirect(url_for('index'))
 
@@ -60,8 +81,10 @@ def edit(id):
 @app.route('/delete/<int:id>', methods=('POST',))
 def delete(id):
     conn = get_db_connection()
-    conn.execute('DELETE FROM accounts WHERE id = ?', (id,))
+    cur = conn.cursor()
+    cur.execute('DELETE FROM accounts WHERE id = %s', (str(id),))
     conn.commit()
+    cur.close()
     conn.close()
     return redirect(url_for('index'))
 
